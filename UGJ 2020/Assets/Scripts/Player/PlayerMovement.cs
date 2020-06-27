646f0d39b3a7d2b7a0ed75ab0a0fd81b3eaf19e8
Ian <russmoonbase@gmail.com>
2020-06-27 00:44:21 -0700

    Added 2D Cartesian and Rotation to player

--- UGJ 2020/Assets/Scripts/Player/PlayerMovement.cs
+++ UGJ 2020/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
    public float moveSpeed;
+   public float rotationSpeed;
 
+   private float newRotationValue;
    private Vector3 inputVector;
    private Vector3 moveVector;
    private float _horizontalInput;
    private float _verticalInput;
+   private float _rightStickHorInput;
    [SerializeField] private string _horizontalInputString;
    [SerializeField] private string _verticalInputString;
+   [SerializeField] private string _rightStickHorInputString;
    [SerializeField] private CharacterController _charController;
    [SerializeField] private Animator _animator; // loaded in Inspector
 
@@ -26,9 +30,12 @@ public class PlayerMovement : MonoBehaviour
    {
       _verticalInput = Input.GetAxisRaw(_verticalInputString);
       _horizontalInput = Input.GetAxisRaw(_horizontalInputString);
+      _rightStickHorInput = Input.GetAxisRaw(_rightStickHorInputString);
+
       inputVector = new Vector3(_horizontalInput, 0f, _verticalInput);
 
       Movement();
+      Turning();
       UpdateAnimations();
    }
 
@@ -45,9 +52,16 @@ public class PlayerMovement : MonoBehaviour
       _charController.Move(moveVector * Time.deltaTime);
    }
 
+   private void Turning()
+   {
+      newRotationValue = _rightStickHorInput * rotationSpeed * Time.deltaTime;
+      transform.Rotate(Vector3.up * newRotationValue);
+   }
+
    private void UpdateAnimations()
    {
       //Debug.Log("Input Magnitude = " + inputVector.magnitude);
-      _animator.SetFloat("LeftStickVert_Input", inputVector.magnitude);
+      _animator.SetFloat("LeftStickVert_Input", _verticalInput);
+      _animator.SetFloat("LeftStickHor_Input", _horizontalInput);
    }
 }
