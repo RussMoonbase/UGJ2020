633f61820a96451bfac8c940c75126fe7d55114c
Ian <russmoonbase@gmail.com>
2020-06-26 21:11:41 -0700

    Player model added and player movement script

---
+++ UGJ 2020/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+   public float moveSpeed;
+
+   private Vector3 moveVector;
+   [SerializeField] private string _horizontalInput;
+   [SerializeField] private string _verticalInput;
+   [SerializeField] private CharacterController _charController;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+      _charController = GetComponent<CharacterController>();
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      Movement();
+   }
+
+   private void Movement()
+   {
+      moveVector = (this.transform.forward * Input.GetAxisRaw(_verticalInput)) + (this.transform.right * Input.GetAxisRaw(_horizontalInput));
+      moveVector.Normalize();
+      moveVector = moveVector * moveSpeed;
+
+      _charController.Move(moveVector * Time.deltaTime);
+   }
+}
