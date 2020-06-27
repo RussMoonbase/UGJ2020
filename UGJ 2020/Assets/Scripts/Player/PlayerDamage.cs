4ef080c077313fa5fa46d0f6d5a2eebc1a1b2f6e
Ian <russmoonbase@gmail.com>
2020-06-27 05:13:08 -0700

    Added ragdool and damage to player

--- UGJ 2020/Assets/Scripts/Player/PlayerDamage.cs
+++ UGJ 2020/Assets/Scripts/Player/PlayerDamage.cs
@@ -4,18 +4,57 @@ using UnityEngine;
 
 public class PlayerDamage : MonoBehaviour
 {
+   public bool testbool = false;
+
    private Rigidbody[] _rigidbodies;
-   private Animator _animator;
+   [SerializeField] private Collider[] _colliders;
+
+   // loaded in Inspector
+   [SerializeField] private Animator _animator;
+   [SerializeField] private CharacterController _charController;
+   [SerializeField] private PlayerMovement _playerMovement;
 
    // Start is called before the first frame update
    void Start()
    {
-
+      _rigidbodies = GetComponentsInChildren<Rigidbody>();
    }
 
    // Update is called once per frame
    void Update()
    {
-
+      if (testbool)
+      {
+         ActivateRagdoll();
+      }
    }
+
+   public void ActivateRagdoll()
+   {
+      if (_playerMovement)
+      {
+         _playerMovement.enabled = false;
+      }
+
+      if (_animator)
+      {
+         _animator.enabled = false;
+      }
+
+      if (_charController)
+      {
+         _charController.enabled = false;
+      }
+
+      foreach (Rigidbody rb in _rigidbodies)
+      {
+         rb.isKinematic = false;
+      }
+
+      foreach (Collider col in _colliders)
+      {
+         col.enabled = true;
+      }
+   }
+
 }
