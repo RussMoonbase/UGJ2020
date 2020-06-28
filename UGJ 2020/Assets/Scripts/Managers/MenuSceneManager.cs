251de302081827af604589f61d4642d34e3e91e6
Ian <russmoonbase@gmail.com>
2020-06-27 22:17:04 -0700

    Main Menu added

---
+++ UGJ 2020/Assets/Scripts/Managers/MenuSceneManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSceneManager : MonoBehaviour
+{
+   public string p1StartButton;
+   public string p2StartButton;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if (Input.GetButtonDown(p1StartButton) || Input.GetButtonDown(p2StartButton))
+      {
+         SceneLoader.instance.LoadNextScene();
+      }
+   }
+}
---
+++ UGJ 2020/Assets/Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+   public static SceneLoader instance;
+
+   private void Awake()
+   {
+      instance = this;
+   }
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
+
+   public void LoadNextScene()
+   {
+      int sceneNum = SceneManager.GetActiveScene().buildIndex;
+      sceneNum++;
+      SceneManager.LoadScene(sceneNum);
+   }
+
+   public void LoadMainMenu()
+   {
+      SceneManager.LoadScene(0);
+   }
+}
